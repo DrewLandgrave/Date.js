b03168e6494e1369c2f12e6559b9564dc3c9459f
Rhonun <drew.landgrave@gmail.com>
2014-06-30 15:54:43 -0400

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,34 @@
+Date.prototype.getMonthName = function (lang) {
+    'use strict';
+    lang = lang && (Date.locale.hasOwnProperty(lang)) ? lang : 'en';
+    return Date.locale[lang].month_names[this.getMonth()];
+};
+
+Date.prototype.getMonthNameShort = function (lang) {
+    'use strict';
+    lang = lang && (Date.locale.hasOwnProperty(lang)) ? lang : 'en';
+    return Date.locale[lang].month_names_short[this.getMonth()];
+};
+
+Date.prototype.getDayWithOrdinal = function () {
+    'use strict';
+    var i  = this.getDay(), j = i % 10;
+    if (j == 1 && i != 11) {
+        return i + "st";
+    }
+    if (j == 2 && i != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && i != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+
+};
+
+Date.locale = {
+    en: {
+        month_names:       ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        month_names_short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    }
+};
